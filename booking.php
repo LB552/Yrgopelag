0baaba7d6587fd36b91c46658dea0d51449bd2bd
Anton Börnfelt <antonb@MacBook-Pro-som-tillhor-Anton.local>
2025-12-12 12:51:29 +0100

    prettier

--- booking.php
+++ booking.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!--DRY?-->
     <meta charset="UTF-8">
@@ -7,24 +8,26 @@
     <link rel="stylesheet" href="style.css">
     <title>Booking</title>
 </head>
+
 <body>
     <div class="dateWall">
-        <?php for ($i=0; $i<5; $i++) {
-            ?>
+        <?php for ($i = 0; $i < 5; $i++) {
+        ?>
             <div class="hori">
-            <?php for ($j=0; $j<7; $j++) {
+                <?php for ($j = 0; $j < 7; $j++) {
                 ?> <div class="date"></div> <?php
-            }
-            ?> </div> <?php
-        }?>
+                                        }
+                                            ?> </div> <?php
+                                                    } ?>
     </div>
     <p>Select room/class:</p>
     <div class="hori">
-    <div class="roomClass">Budget</div>
-    <div class="roomClass">Standard</div>
-    <div class="roomClass">Luxury</div>
+        <div class="roomClass">Budget</div>
+        <div class="roomClass">Standard</div>
+        <div class="roomClass">Luxury</div>
     </div>
     <input class="usernameInput" placeholder="Username"></input>
     <input class="tranferCodeInput" placeholder="transferCode"></input>
 </body>
+
 </html>
