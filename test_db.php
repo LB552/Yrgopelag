1b4ce88f3db65d8d05efd628327cecfb98996339
Anton Börnfelt <antonb@MacBook-Pro-som-tillhor-Anton.local>
2026-01-09 09:49:53 +0100

    new test_db

--- test_db.php
+++ test_db.php
@@ -2,14 +2,18 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+echo "PHP is running<br>";
+
 try {
     $db = new PDO(
         "mysql:host=localhost;dbname=ch1v08l2m_awborndb;charset=utf8mb4",
         "YOUR_MYSQL_USER",
         "YOUR_MYSQL_PASSWORD",
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
     );
     echo "MySQL connection OK";
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "PDO ERROR: " . $e->getMessage();
 }
